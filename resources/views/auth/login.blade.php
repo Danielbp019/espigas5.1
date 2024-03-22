7416d02c20862bf4ab3434527570985428a6127f
Daniel Bautista P <76835848+Danielbp019@users.noreply.github.com>
2024-03-21 22:02:02 -0500

    Nuevo login
    Nuevo login, elimino ayudas que ahora están en modal y formato

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,71 +10,50 @@
 
     <title>Espigas</title>
 
-    <!-- Cargar Bootstrap 3.3.7 CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Cargar Bootstrap 3.4.1 CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
     <!-- Cargar jQuery UI CSS -->
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <!-- Cargar Bootstrap 3.3.7 JS y jQuery -->
+    <!-- Cargar Bootstrap 3.4.1 JS y jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
     <!-- Cargar jQuery UI desde CDN -->
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-    
+
     <!-- Custom CSS -->
-    {!!Html::style('css/small-business.css')!!}
+    {!!Html::style('css/login.css')!!}
     <!--Font Awesome-->
     {!!Html::style('font-awesome-4.6.1/css/font-awesome.min.css')!!}
     <!--favicon-->
-    <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}"/>
+    <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}" />
 
 </head>
 
 <body>
-
-    <!-- Page Content -->
     <div class="container">
         <div class="row">
-            <div class="col-md-4">
-                
-            </div>
-            <!-- /.col-md-4 -->
-            <div class="col-md-4">
-
-                <div class="panel panel-default">
-                    <div class="panel-body">
+            <div class="col-md-offset-4 col-md-4 col-sm-offset-3 col-sm-6">
+                <div class="form-container">
+                    <h3 class="title"><i class="glyphicon glyphicon-log-in"></i> Inicio de Sesión</h3>
+                    {!!Form::open(['route'=>'auth.store', 'method'=>'POST', 'class'=>'form-horizontal', 'autocomplete'=>'off'])!!}
+                    {{ csrf_field() }}
                     @include('errors/loginError')
-                        @include('errors/alertLogin')
-                        {!!Form::open(['route'=>'auth.store', 'method'=>'POST', 'class'=>'form-signin', 'autocomplete'=>'off'])!!} 
-                        {{ csrf_field() }}
-                        <h2 class="form-signin-heading" style=" text-align: center">Inicio de Sesión</h2>
-                        <label for="inputEmail" class="sr-only">Nombre de usuario</label>
-                        {!!Form::text('name',null,['class'=>'form-control','placeholder'=>'Nombre de usuario', 'autofocus'])!!}
-                        <br/>
-                        <label for="inputPassword" class="sr-only">Contraseña</label>
-                    {!!Form::password('password',['class'=>'form-control', 'placeholder'=>'Ingresa tu contraseña'])!!}
-
-                        <!--<div class="checkbox">
-                        <label>
-                            <input type="checkbox" value="remember-me"> Remember me
-                        </label>
-                    </div>-->
-                        <br/>
-                        <button class="btn btn-lg btn-primary btn-block" type="submit"><i class="fa fa-key" aria-hidden="true"> Iniciar</i></button>
-                        {!! Form::close() !!}
+                    @include('errors/alertLogin')
+                    <div class="form-group">
+                        <label for="">Usuario</label>
+                        {!!Form::text('name',null,['class'=>'form-control','placeholder'=>'Nombre de usuario', 'required', 'autofocus'])!!}
                     </div>
+                    <div class="form-group">
+                        <label for="">Contraseña</label>
+                        {!!Form::password('password',['class'=>'form-control', 'placeholder'=>'Ingresa tu contraseña', 'required'])!!}
+                    </div>
+                    <button class="btn signup" type="submit">Iniciar</button>
+                    {!! Form::close() !!}
                 </div>
             </div>
-            <!-- /.col-md-4 -->
-            <div class="col-md-4">
-
-            </div>
-            <!-- /.col-md-4 -->
         </div>
     </div>
-    <!-- /.container -->
 
-    </div>
-    <!-- /.container -->
 </body>
 
 </html>
--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -9,13 +9,13 @@
     <meta name="author" content="Daniel Bautista P., col.">
     <title>Espigas</title>
 
-    <!-- Cargar Bootstrap 3.3.7 CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Cargar Bootstrap 3.4.1 CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
     <!-- Cargar jQuery UI CSS -->
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <!-- Cargar Bootstrap 3.3.7 JS y jQuery -->
+    <!-- Cargar Bootstrap 3.4.1 JS y jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
     <!-- Cargar jQuery UI desde CDN -->
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 
